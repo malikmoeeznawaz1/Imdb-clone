612df5c7ea7ac914f1b2b1ddb0786ca345ca8139
Malik Moeez Nawaz <2021se30@student.uet.edu.pk>
2025-10-30 14:45:56 +0500

    data is proply called in riht side of section and slider is adjusted as well

--- src/Components/ComingNext.jsx
+++ src/Components/ComingNext.jsx
@@ -1,78 +1,47 @@
-import uimg from '../assets/uimg.jpg';
 import { GoPlay } from "react-icons/go";
 import { IoIosStarOutline } from "react-icons/io";
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchData } from '../Data/slice';
+import { useEffect } from "react";
 
 
-const ComingNext = () => {    
+const ComingNext = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(fetchData());
+    }, []);
+
+    const data = useSelector((state) => state.data.data);
 
     return (
-        <div className='w-full h-full md:flex flex-col py-2 justify-start hidden'>
+        <div className='w-full h-full md:flex flex-col py-2 justify-start hidden '>
             <span className='text-xl font-bold text-[#F5C518] mb-4'>Up Next</span>
-            <div className='px-2 lg:px-4 py-2'>
-                <div className='flex gap-3'>
-                    <div className="shrink-0">
-                        <img src={uimg} alt="Upcoming Movie" className='w-20 md:w-28 h-28 md:h-36 object-cover rounded-2xl' />
+            {
+                data.map((item) => {
+                    return <div key={item.id} className='px-2 lg:px-4 py-2'>
+                        <div className='flex gap-3'>
+                            <div className="shrink-0">
+                                <img src={item.image.medium} alt="Upcoming Movie" className='w-20 md:w-28 h-28 md:h-36 object-cover rounded-2xl' />
+                            </div>
+                            <div className='flex-1 min-w-0 flex flex-col cursor-pointer'>
+                                <div className="flex gap-2 items-center hover:text-[#F5C518]">
+                                    <GoPlay size={32} className='font-light shrink-0' />
+                                    <span className='flex items-end pb-1 font-sm text-[#9DA6AE] text-sm'>{`${Math.floor(item.runtime / 60)}h ${item.runtime % 60}m`}</span>
+                                </div>
+                                <div className='mt-2 flex-1'>
+                                    <p className='text-sm lg:text-md font-sm truncate'>'{item.name}'</p>
+                                    <p className='text-sm lg:text-md font-sm text-[#B7B7B7] truncate'>{item.genres[0]}, {item.genres[1]}, {item.genres[2]}</p>
+                                </div>
+                                <div className='flex items-center gap-2 py-3 lg:py-6'>
+                                    <IoIosStarOutline className='text-[#F5C518] text-lg shrink-0' />
+                                    <span className='text-sm'>{item.rating.average}</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className='flex-1 min-w-0 flex flex-col cursor-pointer'>
-                        <div className="flex gap-2 items-center hover:text-[#F5C518]">
-                            <GoPlay size={32} className='font-light shrink-0' />
-                            <span className='flex items-end pb-1 font-sm text-[#9DA6AE] text-sm'>2:23</span>
-                        </div>
-                        <div className='mt-2 flex-1'>
-                            <p className='text-sm lg:text-md font-sm truncate'>'Movie Title Here'</p>
-                            <p className='text-sm lg:text-md font-sm text-[#B7B7B7] truncate'>Watch the trailer</p>
-                        </div>
-                        <div className='flex items-center gap-2 py-3 lg:py-6'>
-                            <IoIosStarOutline className='text-[#F5C518] text-lg shrink-0' />
-                            <span className='text-sm'>6.5</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                })
+            }
 
-            <div className='px-2 lg:px-4 py-2'>
-                <div className='flex gap-3'>
-                    <div className="shrink-0">
-                        <img src={uimg} alt="Upcoming Movie" className='w-20 md:w-28 h-28 md:h-36 object-cover rounded-2xl' />
-                    </div>
-                    <div className='flex-1 min-w-0 flex flex-col cursor-pointer'>
-                        <div className="flex gap-2 items-center">
-                            <GoPlay size={32} className='font-light shrink-0' />
-                            <span className='flex items-end pb-1 font-sm text-[#9DA6AE] text-sm'>2:23</span>
-                        </div>
-                        <div className='mt-2 flex-1'>
-                            <p className='text-sm lg:text-md font-sm truncate'>'Movie Title Here'</p>
-                            <p className='text-sm lg:text-md font-sm text-[#B7B7B7] truncate'>Watch the trailer</p>
-                        </div>
-                        <div className='flex items-center gap-2 py-3 lg:py-6'>
-                            <IoIosStarOutline className='text-[#F5C518] text-lg shrink-0' />
-                            <span className='text-sm'>6.5</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className='px-2 lg:px-4 py-2'>
-                <div className='flex gap-3'>
-                    <div className="shrink-0">
-                        <img src={uimg} alt="Upcoming Movie" className='w-20 md:w-28 h-28 md:h-36 object-cover rounded-2xl' />
-                    </div>
-                    <div className='flex-1 min-w-0 flex flex-col cursor-pointer'>
-                        <div className="flex gap-2 items-center">
-                            <GoPlay size={32} className='font-light shrink-0' />
-                            <span className='flex items-end pb-1 font-sm text-[#9DA6AE] text-sm'>2:23</span>
-                        </div>
-                        <div className='mt-2 flex-1'>
-                            <p className='text-sm lg:text-md font-sm truncate'>'Movie Title Here'</p>
-                            <p className='text-sm lg:text-md font-sm text-[#B7B7B7] truncate'>Watch the trailer</p>
-                        </div>
-                        <div className='flex items-center gap-2 py-3 lg:py-6'>
-                            <IoIosStarOutline className='text-[#F5C518] text-lg shrink-0' />
-                            <span className='text-sm'>6.5</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     )
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,9 +4,9 @@ import ComingNext from "../Components/ComingNext";
 const Home = () => {
     return (
         <div className="w-full min-h-screen text-white overflow-hidden bg-black pt-4">
-            <div className="flex max-w-7xl mx-auto p-2 gap-2">
+            <div className="flex max-w-7xl mx-auto pt-2 gap-2">
                 {/* Left side - main trailer area */}
-                <div className="flex flex-1 md:h-[500px] h-[300px]  w-2/3 p-2 ">
+                <div className="flex flex-1 md:h-[500px] h-[300px]  w-2/3 p-2">
                     <Carousel />
                 </div>
 
