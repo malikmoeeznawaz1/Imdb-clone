500aaa7cceacb6d7da8d79dc3f5ce57a125e844b
Malik Moeez Nawaz <2021se30@student.uet.edu.pk>
2025-10-30 21:26:35 +0500

    This is clean updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Home from './Pages/Home'
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
